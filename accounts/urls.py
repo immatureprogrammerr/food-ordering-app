c438be8b73b621d1807570524d7b80e6e44e7eba
Paras Sahu <immatureprogrammer@gmail.com>
2025-07-13 18:13:40 +0530

    Completed Vendor Registration Flow.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registerUser/', views.register_user, name='registerUser')
+    path('registerUser/', views.register_user, name='registerUser'),
+    path('registerRestaurant/', views.register_restaurant, name='registerRestaurant'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 # Create your views here.
@@ -37,3 +39,46 @@ def register_user(request):
         'form': form,
     }
     return render(request, 'accounts/registeruser.html', context)
+
+def register_restaurant(request):
+    if request.method == 'POST':
+        #store the data and create the user
+        form = UserForm(request.POST)
+        vendor_form = VendorForm(request.POST, request.FILES)
+
+        if form.is_valid() and vendor_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+            )
+            user.role = User.RESTAURANT
+            user.save()
+
+            vendor = vendor_form.save(commit=False)
+            vendor.user = user
+
+            # getting the user profile from the user which got created via Signals
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'You have registered successfully. Please wait for the Admin approval.')
+            return redirect('registerRestaurant')
+        else:
+            print(form.errors)
+    else:
+        form = UserForm()
+        vendor_form = VendorForm()
+
+    context = {
+        'form': form,
+        'vendor_form': vendor_form,
+    }
+    return render(request, 'accounts/registerrestaurant.html', context=context)
